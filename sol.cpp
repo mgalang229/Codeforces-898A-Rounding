ec8330771dda0c9a07701f5a7fade1f9646d52b3
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-06 21:33:14 +0800

    Update sol.cpp

--- sol.cpp
+++ sol.cpp
@@ -1,29 +1,28 @@
 #include <bits/stdc++.h>
-
+#include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
+using namespace __gnu_pbds;
+
+#define ll long long
+#define ar array
+
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> indexed_set;
 
 int main() {
-	ios::sync_with_stdio(false);
+	ios::sync_with_stdio(0);
 	cin.tie(0);
+	
 	int n;
 	cin >> n;
-	int tmp1 = n;
-	int tmp2 = n;
-	int cnt1 = 0;
-	int cnt2 = 0;
-	while (tmp1 % 5 != 0 || tmp1 % 10 != 0) {
+	int tmp1=n, tmp2=n, cnt1=0, cnt2=0;
+	while(tmp1%5!=0||tmp1%10!=0) {
 		tmp1--;
 		cnt1++;
 	}
-	while (tmp2 % 5 != 0 || tmp2 % 10 != 0) {
+	while(tmp2%5!=0||tmp2%10!=0) {
 		tmp2++;
 		cnt2++;
 	}
-	if (cnt1 <= cnt2) {
-		cout << n - cnt1;
-	} else {
-		cout << n + cnt2;
-	}
-	cout << '\n';
-	return 0;
+	cout << (cnt1<=cnt2?n-cnt1:n+cnt2) << "\n";
 }
