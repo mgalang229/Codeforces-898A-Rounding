fc575ad1687f434f54f9d56f83c1bfe9d435445d
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-06 21:35:00 +0800

    Update optimized.cpp

--- optimized.cpp
+++ optimized.cpp
@@ -1,19 +1,21 @@
 #include <bits/stdc++.h>
-
+#include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
+using namespace __gnu_pbds;
+
+#define ll long long
+#define ar array
+
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> indexed_set;
 
 int main() {
-	ios::sync_with_stdio(false);
+	ios::sync_with_stdio(0);
 	cin.tie(0);
+	
 	int n;
 	cin >> n;
-	int a = (n / 10) * 10;
-	int b = a + 10;
-	if (n - a > b - n) {
-		cout << b;
-	} else {
-		cout << a;
-	}
-	cout << '\n';
-	return 0;
+	int a=(n/10)*10;
+	int b=a+10;
+	cout << (n-a>b-n?b:a) << "\n";
 }
